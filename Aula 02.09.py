e8d56ea75024c7d770b610b4a8f686f5e19bdf60
Angelo-Soares <angelosoaresjunior23@gmail.com>
2025-09-02 21:09:07 -0300

    teste

---
+++ Aula 02.09.py
@@ -0,0 +1,37 @@
+#Aula
+#Tema: Loop
+
+'''MAXIMO = 15
+for i in numbers:
+    print(i)
+    numbers.sort()
+    print(numbers)'''
+
+'''MAXIMO = 10
+    i = 0
+    while (i < 10):
+        i = i + 1
+        print(i)'''
+
+
+'''# Declarações
+alunos = []
+notas = []
+MEDIA = 6.0
+MINIMO = 3.0
+
+for i in range(5):
+    aluno = input('Digite o nome do aluno:')
+    nota = float(input('Digite a nota do aluno:'))
+    alunos.append(aluno)
+    notas.append(nota)
+
+    if(float(nota)>=MEDIA):
+        print(f'O aluno está APROVADO com nota {nota}')
+    elif (float(nota)>=MINIMO):
+        print(f'O aluno está RECUPERAÇÃO com nota {nota}')
+    else:
+        print(f'O aluno está REPROVADO com nota {nota}')
+
+print(alunos)
+print(notas)'''
---
+++ Exercicio 1.py
@@ -0,0 +1,1 @@
+#Criar um algoritmo para ler os lados de um triângulo e identificar se é equilátero.
